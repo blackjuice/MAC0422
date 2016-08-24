0fb4320c7ed8d0bdd4e3789b736cdd5d9bc02947
BlackJuice <lucas.sj.hong@gmail.com>
2016-08-24 16:48:42 -0300

    1st test

---
+++ basic_test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define TRUE 1
+
+int main () {
+
+    while (TRUE){
+         type_prompt();
+    /* repeat forever */
+    /* display prompt on the screen */ /* read input from terminal */
+    /* fork off child process */ /* wait for child to exit */
+    }
+    read_command(command, parameters);
+    if (fork() != 0){
+         /* Parent code. */
+         waitpid(1, &status, 0);
+    } else {
+        /* Child code. */
+    execve(command, parameters, 0); /* execute command */
+
+}
