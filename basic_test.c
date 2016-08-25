7cb032375d756b3984220c7bfe36f9f418087a5a
Lucas Sung Jun Hong <lucassjhong@scheme.linux.ime.usp.br>
2016-08-25 17:22:14 -0300

    formatacao ate rodeveja

--- basic_test.c
+++ basic_test.c
@@ -1,3 +1,10 @@
+/*
+ * EP1 - MAC0422 - SISTEMAS OPERACIONAIS
+ * alunos:  Leonardo Daneu Lopes;   no.USP: 8516816
+ *          Lucas Sung Jun Hong;    no.USP: 8124329
+ *
+*/ 
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,19 +12,24 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include "sys/stat.h"
-#define TRUE  1
-#define FALSE 0
+#define TRUE        1
+#define FALSE       0
+#define DELIM       " \n"
+#define MAX_LENGTH  100
+#define BUFFSIZE    20
 
-#define DELIM " \n"
-#define MAX_LENGTH 100
-#define BUFFSIZE 20
+/*-------------------------------------------------*/
+/*-----------DECLARACAO DAS FUNCOES----------------*/
+/*-------------------------------------------------*/
+int protegepracaramba   ( char *filename );
+int liberageral         ( char *filename );
+int rodeveja            ( char **comando );
+void free_tokens        ( char **tokens );
 
-/*  */
-int protegepracaramba( char *filename );
-int liberageral ( char *filename );
-int rodeveja (char **comando);
-
-int protegepracaramba( char *filename ) {
+/*-------------------------------------------------*/
+/*-------------------FUNCOES-----------------------*/
+/*-------------------------------------------------*/
+int protegepracaramba ( char *filename ) {
     return syscall(SYS_chmod, filename, 0);
 }
 
@@ -25,75 +37,80 @@ int liberageral ( char *filename ) {
     return syscall(SYS_chmod, filename, 0777);    
 }
 
-//int rodeveja (char * comando, char * parametro[], char *filename, char * env[]) {
 int rodeveja (char **comando) {
-
-    //execve( filename, argv, envp);
-    int status;
-    int tmp;
-
-    //char *newargv[] = 
-    char *const parametro[] = { NULL };
+    int         status;
+    int         tmp;
     char *const newenviron[] = { NULL };
 
+    // processo pai
     if (fork() != 0){
-        /* Parent code. */
          wait(&status);
     } 
+    // processo filho
     else {
-        /* Child code. */
-        printf ("%s", comando[0]);
         tmp = execve(comando[0], comando, newenviron);
         printf("programa %s retorna %d\n", comando[0], tmp);
-        printf ("%d", tmp);
+        printf("%d", tmp);
     }
     return 0;
 }
 
-int main(int argc, char * const argv[], char * const envp[]) 
-{
-    char line_tudo[MAX_LENGTH];
-    int processo, i, j;
-    char * tmp, * tmp1;
-    char **tokens;
-    char * const rapidinho[] = {NULL};
+/*-------------------------------------------------*/
+/*-------------FUNCOES AUXILIARES------------------*/
+/*-------------------------------------------------*/
+void free_tokens (char **tokens) {
+    int i;
+
+    for (i = 0; i < BUFFSIZE; i++)
+        free(tokens[i]);
+}
+
+/*-------------------------------------------------*/
+/*----------------------MAIN-----------------------*/
+/*-------------------------------------------------*/
+int main () {
+    char    line_tudo[MAX_LENGTH];
+    int     processo, i, j;
+    char    * tmp, * tmp1;
+    char    **tokens;
 
     while (1) {
         printf("$ ");
+
         if (!fgets(line_tudo, MAX_LENGTH, stdin)) break;
         
         tmp = strtok (line_tudo, DELIM);
+
+        /* CONDICOES */
+        /* protegepracaramba <caminho do arquivo> */
         if (!strcmp("protegepracaramba", tmp)) {
-            printf("entrou protegepracaramba\n");
-            tmp = strtok (NULL, DELIM);
-            processo = protegepracaramba(tmp);
+            tmp         = strtok (NULL, DELIM);
+            processo    = protegepracaramba(tmp);
         }
+
+        /* liberageral <caminho do arquivo> */
         if (!strcmp("liberageral", tmp)) {
-            printf("entrou liberageral\n");
-            tmp = strtok (NULL, DELIM);
-            processo = liberageral(tmp);
+            tmp         = strtok (NULL, DELIM);
+            processo    = liberageral(tmp);
         }
+
+        /* rodeveja <caminho do programa> */
         if (!strcmp("rodeveja", tmp)) {
-            i = 0;  
-            tokens = malloc(50 * sizeof(char*));
-            tmp = strtok(NULL, DELIM);
+            tokens  = malloc(BUFFSIZE * sizeof(char*));
+            tmp     = strtok(NULL, DELIM);
 
-            while(tmp != NULL) {
-                tokens[i] = malloc(strlen(tmp) * sizeof(char));
+            for (i = 0; tmp != NULL; i++) {
+                tokens[i]   = malloc(strlen(tmp) * sizeof(char));
                 strcpy(tokens[i], tmp);
-                i++;
-                tmp = strtok(NULL, DELIM);
+                tmp         = strtok(NULL, DELIM);
             }
-            tokens[i] = NULL;
-            processo = rodeveja(tokens);
+            tokens[i]   = NULL;
+            processo    = rodeveja(tokens);
 
-            for (i = 0; i < 50; i++)
-                free(tokens[i]);
-            
+            free_tokens(tokens);
         }
         //else 
           //  system(line_tudo);
     }
-
     return 0;
 }
